94121c7807e6002b6b89bb3c109a259c083f3189
LightLight <Cobalttft@yandex.ru>
2022-12-10 20:34:16 +0300

    rewriting

--- src/codewars/AreTheyTheSame.java
+++ src/codewars/AreTheyTheSame.java
@@ -1,32 +1,19 @@
 package codewars;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class AreTheyTheSame {
 	public static boolean comp(int[] a, int[] b) {
-		boolean result = true;
-		if (a == null || b == null) {
-			result = false;
-		} else {
-			Arrays.sort(a);
-			Arrays.sort(b);
-
-			for (int i = 0; i < a.length; i++) {
-				if (a[i] * a[i] != b[i]) {
-					result = false;
-					break;
-				}
-
-			}
-		}
+		boolean result;
+		List<int[]> collectionA = Arrays.asList(a);
+		List<int[]> collectionB = Arrays.asList(b);
+		collectionA.stream().anyMatch(collectionB);
 		return result;
 	}
-
 	public static void main(String[] args) {
-		int[] a = null;
-		int[] b = null;
+		int[] a = {2,3,4,5};
+		int[] b = {25,4,25,9,16,0};
 		System.out.println(comp(a, b));
-
 	}
-
 }
